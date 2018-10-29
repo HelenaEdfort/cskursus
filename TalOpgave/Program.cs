1a32712e6e2a69ebfaefa4508e326a109dd2ccd6
Helena <admin@TA-828-06>
2018-10-29 15:53:29 +0100

    Opgave tal

---
+++ TalOpgave/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int a;
+            a = 10;
+
+            a++;
+
+            Console.WriteLine($"a = {a}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Variabeldemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variabeldemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a;
+
+            a = "test";
+
+            Console.WriteLine(a);
+                
+
+            Console.ReadLine();
+
+        }
+    }
+}
