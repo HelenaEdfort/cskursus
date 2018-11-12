4e7a65813813ca5ba7e3af4eabc03a9da4e838c5
Helena <edfortpetersen@hotmail.com>
2018-11-12 14:46:57 +0100

    Tilføjet nye opgaver

---
+++ FejlEgenException/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FejlEgenException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Først checke om filen c:\temp\tal.txt eksisterer (System.IO.File.Exists).
+
+            bool findes;
+            findes = System.IO.File.Exists("c:\\temp\\tal.txt");
+
+            Console.WriteLine($"Findes filen = {findes}");
+
+            // Hvis den ikke findes skal der smides en ApplikationException med teksten "Filen c:\temp\tal.txt findes ikke!"
+
+            // if (!System.IO.File.Exists("c:\\temp\\tal.txt"))
+            //    throw new ApplicationException("Filen c:\\temp\\tal.txt findes ikke");
+
+            // I en try/catch-struktur skal du nu: Åbne filen og hent indhold til en streng(System.IO.File.ReadAllText).Hvis det går galt skal fejlen blot smides videre.
+
+            String filIndhold = "";
+
+            try
+            {
+                filIndhold = System.IO.File.ReadAllText("c:\\temp\\tal.txt");
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+
+
+
+        }
+    }
+}
---
+++ MetodeNullable/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodeNullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tilføj en metode HentFraDb der tager en int som argument og return en nullable bool. Metoden simulerer kald til en database efter et felt med en mulig null-værdi
+            // Hvis int = 1 returneres true
+            // Hvis int = 2 restureres false
+            // Ellers returneres null
+
+            bool? test;
+
+            test = HentFraDb(1);
+            Console.WriteLine($"null = {test}");
+
+            test = HentFraDb(2);
+            Console.WriteLine($"null = {test}");
+
+            test = HentFraDb(5);
+            Console.WriteLine($"null = {test}");
+            
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+                
+            }
+
+        }
+
+        static bool? HentFraDb(int a)
+        {
+            if (a == 1)
+                return true;
+            if (a == 2)
+                return false;
+            return null;
+        }
+    }
+}
