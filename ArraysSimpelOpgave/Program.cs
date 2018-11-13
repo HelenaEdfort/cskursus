b90c299a89729bd9e316c0453a28f97d27136da8
Helena <edfortpetersen@hotmail.com>
2018-11-13 15:06:00 +0100

    Tilføjet opgaver vedr. Arrays

---
+++ ArraysSimpelOpgave/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysSimpelOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I en tom konsol-app skal du oprette et (int) array med et antal tilfældige månedslønninger, udskrive disse samt et beregne et gennemsnit.
+
+            int[] ml = { 3000, 7000, 5000, 6000 };
+            int sum = 0;
+            for (int i = 0; i < ml.Length; i++)
+            {
+                sum += ml[i];
+                Console.WriteLine(ml[i]);
+            }
+//            Console.WriteLine($"summen er {sum:N2}");
+            double gns = sum / ml.Length;
+            Console.WriteLine($"Gennemsnittet er {gns:N2}");
+
+            int Sortering(int[] array)
+            {
+                System.Array.Sort(array);
+                return array[0];
+            }
+
+            Console.WriteLine($"Mindsteværdi er : {Sortering(ml)}");
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ArraysSplitOpgave/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysSplitOpgave
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            // Du skal nu ved hjælp af Split-funktion (txt.Split) splitte strenge til et string-array (string[]). 
+            // Herefter skal du løbe arrayet igennem, konvertere hvert element til et tal (int), og finde summen af samtlige tal og udskrive denne.
+            string txt = "3;5;22;1;10;15;5";
+
+            string[] numre = txt.Split(';');
+
+            int sum = 0;
+
+            for (int i = 0; i < numre.Length; i++)
+            {
+                int tal = Convert.ToInt32(numre[i]);
+                sum += tal;
+            }
+            Console.WriteLine($"Summen er = {sum:N0}");
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
